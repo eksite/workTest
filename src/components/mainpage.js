c8c65395267c55cfc495af0f9c5ce24818b75af1
SergeyL <ken4o@mail.ru>
2019-10-28 17:58:22 +0200

    initial commit

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -15,7 +15,8 @@ class Mainpage extends React.Component {
             name: '',
             room: '',
             // socket: props.socket,
-            messageMode: false
+            messageMode: false,
+
         }
     }
 
@@ -28,12 +29,11 @@ class Mainpage extends React.Component {
         this.setState({ room: e.target.value })
     }
 
-    submit(e) {
+    submit = (e) => {
         e.preventDefault();
         if (this.state.name.length > 0 && this.state.room.length > 0) {
             socket.emit("join Room", { username: this.state.name, room: this.state.room }, (data) => {
                 if (data.success) {
-                    console.log("hi")
                     this.setState({ messageMode: true })
                 }
             })
@@ -43,7 +43,7 @@ class Mainpage extends React.Component {
     render() {
         if (!this.state.messageMode) {
             return (
-                <form onSubmit={this.submit.bind(this)} >
+                <form onSubmit={this.submit} >
                     <input id="name" onChange={this.handleInputName} value={this.state.name} />
                     <input id="room" onChange={this.handleInputRoom} value={this.state.room} />
                     <button type="submit"></button>
--- src/components/messanger.js
+++ src/components/messanger.js
@@ -8,24 +8,42 @@ class Messanger extends React.Component {
         super(props);
         this.state = {
             message: '',
-            socket: props.socket
+            socket: props.socket,
+            messages: []
         }
+    }
+    componentDidMount() {
+        this.state.socket.on('message', (data) => {
+            this.state.messages.push(data);
+            console.log(this.state.messages);
+        })
     }
 
     handleInput = (e) => {
         this.setState({ message: e.target.value })
     }
-    submit(e) {
+
+
+    submit = (e) => {
         e.preventDefault();
-        this.state.socket.emit('chat message', { msg: this.state.message })
+        this.state.socket.emit('chat message', this.state.message, () => {
+            console.log(this.state.messages.length)
+        })
     }
 
     render() {
         return (
-            <form onSubmit={this.submit}>
-                <input type="text" value={this.state.message} id="message" onChange={this.handleInput} />
-                <button type="submit" value="Submit" />
-            </form>
+            <div>
+                <form onSubmit={this.submit}>
+                    <input type="text" value={this.state.message} id="message" onChange={this.handleInput} />
+                    <button type="submit" value="Submit" />
+                </form>
+                {
+                    this.state.messages.map((message, index) => (
+                        <li key={index}>{message.msg}</li>
+                    ))
+                }
+            </div >
         )
     }
 
