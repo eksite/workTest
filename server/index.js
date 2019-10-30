a0c20ddaa3b481e61139b175e5a73e91072d6a28
SergeyL <ken4o@mail.ru>
2019-10-30 22:23:56 +0200

    delete useless files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
 let connUsers = {};
 
 io.on('connection', socket => {
